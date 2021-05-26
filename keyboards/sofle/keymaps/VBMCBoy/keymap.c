e9af4fe2a9fd8c9ffd589537e3cabd09360ce588
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2021-05-26 10:28:23 +0200

    modified sofle keymap heavily

--- keyboards/sofle/keymaps/VBMCBoy/keymap.c
+++ keyboards/sofle/keymaps/VBMCBoy/keymap.c
@@ -25,59 +25,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  OMNU |    | OSCT  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | GAME | LALT | /Enter  /       \Space \  | SYMB | RCTR | RAlt | RGUI |
+ *            | LCTR | LGUI | SYMB | LALT | /Enter  /       \Space \  | GAME | RCTR | MENU | RGUI |   Menu works as the compose key
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 
 [_QWERTY] = LAYOUT( \
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_OMNU,   KC_OSCT, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LCTRL,KC_LGUI,KC_GAME,  KC_LALT, KC_ENT,      KC_SPC,  KC_SYMB,  KC_RCTRL, KC_RALT, KC_RGUI \
+  KC_GRV,   KC_1,   KC_2,    KC_3,     KC_4,    KC_5,                      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,                      KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
+  KC_ESC,   KC_A,   KC_S,    KC_D,     KC_F,    KC_G,                      KC_H,       KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,     KC_V,    KC_B, KC_OMNU,   KC_OSCT,  KC_N,       KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
+                 KC_LCTRL,KC_LGUI,TO(_SYMB),  KC_LALT, KC_ENT,      KC_SPC, TO(_GAME),  KC_RCTRL, KC_APP, KC_RGUI \
 ),
-/* SYMB
+/* GAME
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |   1  |   2  |   3  |   4  |   5  |                    |      |      |   /  |   *  |   -  |      |
+ * | TRNS |   1  |   2  |   3  |   4  |   5  |                    |      |      |   /  |   *  |   -  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |      |   7  |   8  |   9  |   +  |      |
+ * | TRNS |   Q  |   W  |   E  |   R  |   T  |                    |      |   7  |   8  |   9  |   +  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|      |   4  |   5  |   6  |   ,  |      |
  * |------+------+------+------+------+------| TRNS  |    | TRNS  |------+------+------+------+------+------|
  * |LCtrl |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|      |   1  |   2  |   3  |   =  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |QWERTY| LALT | /Space  /       \      \  |   0  |   0  |   .  | Enter|
+ *            | TRNS | TRNS | TRNS | TRNS | /Space  /       \ ESC  \  |QWERTY|   0  |   .  | Enter|
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_GAME] = LAYOUT( \
-  _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                       _______,   _______,  KC_PSLS,   KC_PAST,  KC_PMNS,  _______,\
-  KC_GRV,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       _______,   KC_P7,    KC_P8,     KC_P9,    KC_PPLS,  _______, \
-  _______,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                       _______,   KC_P4,    KC_P5,     KC_P6,    KC_PCMM,  _______, \
-  _______,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, _______,     _______, _______,   KC_P1,    KC_P2,     KC_P3,    KC_PEQL,  _______, \
-        _______, _______, _______, _______, _______,                 _______, _______, _______, _______, _______\
+  _______,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                       XXXXXXX,   XXXXXXX,  KC_PSLS,   KC_PAST,  KC_PMNS,  XXXXXXX,\
+  _______,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       XXXXXXX,   KC_P7,    KC_P8,     KC_P9,    KC_PPLS,  KC_TRNS, \
+  KC_LSFT,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                       XXXXXXX,   KC_P4,    KC_P5,     KC_P6,    KC_PCMM,  XXXXXXX, \
+  KC_LCTRL,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, _______,     _______, XXXXXXX,   KC_P1,    KC_P2,     KC_P3,    KC_PEQL,  KC_TRNS, \
+        _______, _______, _______, _______, _______,                 _______, TO(_QWERTY), KC_P0, KC_PDOT, KC_PENT\
 ),
-/* GAME
+/* SYMB
  * ,----------------------------------------.                    ,-----------------------------------------.
- * |      | F1   | F2   | F3   | F4   | F5   |                    | F6   | F7   |  F8  | F9   | F10  | F11   |
+ * | TRNS | F1   | F2   | F3   | F4   | F5   |                    | F6   | F7   |  F8  | F9   | F10  | F11   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Esc  | Ins  | Pscr | Menu |      |      |                    |      |PrntSc|ScrLck|Pause |      | F12  |
+ * | TRNS |      |      |      |      |      |                    |  R   |  E   |PrntSc|ScrLck|Pause | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.    ,-------|      | Ins  | Home | PgUp |      | Bspc |
- * |------+------+------+------+------+------|  MUTE  |    |       |------+------+------+------+------+------|
- * |Shift | Undo |  Cut | Copy | Paste|      |-------|    |-------|      | Del  | End  | PgDn |      | TRNS |
+ * | TRNS | LEFT |  UP  | DOWN | RIGHT|      |-------.    ,-------|  I   |  S   | Ins  | Home | PgUp | Bspc |
+ * |------+------+------+------+------+------| TRNS   |   | TRNS  |------+------+------+------+------+------|
+ * | TRNS |      |      |      |      |      |-------|    |-------|  U   |  B   | Del  | End  | PgDn | TRNS |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |SYMB | /Enter  /       \Space \  |GAME | RCTR | RAlt | RGUI |
+ *            | TRNS | LAlt |QWERTY|     |  /Enter  /       \Space \  |      | TRNS | TRNS | TRNS |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 [_SYMB] = LAYOUT( \
-  _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______, \
-  _______,  KC_INS,  KC_PSCR,   KC_APP,  XXXXXXX, XXXXXXX,                        KC_PGUP, XXXXXXX,   KC_UP, XXXXXXX,XXXXXXX, KC_BSPC, \
-  _______, KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL, KC_BSPC, \
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,       _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, _______, \
-                         _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______ \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                         KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                       KC_R,  KC_E,  KC_PSCR, KC_SLCK, KC_PAUS, KC_F12, \
+  _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, XXXXXXX,                       KC_I,  KC_S,  KC_INS,  KC_HOME, KC_PGUP, KC_BSPC, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  _______,    _______, KC_U,  KC_B,  KC_DEL,  KC_END,  KC_PGDN, _______, \
+        _______, _______, TO(_QWERTY), _______, KC_ENT,                      KC_SPC, XXXXXXX, _______, _______, _______ \
 ),
 };
 
