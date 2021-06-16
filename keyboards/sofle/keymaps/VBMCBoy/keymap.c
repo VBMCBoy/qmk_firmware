419c2315d983888b98cf1340a8b1d580f85b417d
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2021-06-16 14:58:48 +0200

    swapped layer switch and compose key

--- keyboards/sofle/keymaps/VBMCBoy/keymap.c
+++ keyboards/sofle/keymaps/VBMCBoy/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,                      KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_ESC,   KC_A,   KC_S,    KC_D,     KC_F,    KC_G,                      KC_H,       KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,     KC_V,    KC_B, KC_OMNU,   KC_OSCT,  KC_N,       KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LCTRL,TO(_SYMB), KC_LGUI,  KC_LALT, KC_ENT,      KC_SPC, TO(_GAME),  KC_ALGR, KC_APP, KC_RGUI \
+                 KC_LCTRL,TO(_SYMB), KC_LGUI,  KC_LALT, KC_ENT,      KC_SPC, KC_APP,  KC_ALGR, TO(_GAME), KC_RGUI \
 ),
 /* GAME
  * ,-----------------------------------------.                    ,-----------------------------------------.
