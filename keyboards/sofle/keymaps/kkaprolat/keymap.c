46e5afb0044c1127b63bf2e17e922b575b969062
Kay Kaprolat <kakaoh6@gmail.com>
2024-04-08 20:22:56 +0200

    update sofle keymap

--- keyboards/sofle/keymaps/kkaprolat/keymap.c
+++ keyboards/sofle/keymaps/kkaprolat/keymap.c
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,     KC_4,    KC_5,                      KC_6,       KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,     KC_R,    KC_T,                      KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_ESC,   KC_A,   KC_S,    KC_D,     KC_F,    KC_G,                      KC_H,       KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
-  KC_LSPO,  KC_Z,   KC_X,    KC_C,     KC_V,    KC_B, KC_OMNU,   KC_OSCT,  KC_N,       KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSPC, \
-                 KC_LCTRL,TO(_SYMB), KC_LALT,  KC_LGUI, KC_ENT,      KC_SPC, KC_APP,  KC_ALGR, TO(_GAME), KC_RGUI \
+  SC_LSPO,  KC_Z,   KC_X,    KC_C,     KC_V,    KC_B, KC_OMNU,   KC_OSCT,  KC_N,       KC_M, KC_COMM,  KC_DOT, KC_SLSH,  SC_RSPC, \
+                 KC_LCTL,TO(_SYMB), KC_LALT,  KC_LGUI, KC_ENT,      KC_SPC, KC_APP,  KC_ALGR, TO(_GAME), KC_RGUI \
 ),
 /* GAME
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_GAME] = LAYOUT( \
   _______,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                       XXXXXXX,   XXXXXXX,  KC_PSLS,   KC_PAST,  KC_PMNS,  XXXXXXX,\
   _______,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       XXXXXXX,   KC_P7,    KC_P8,     KC_P9,    KC_PPLS,  KC_TRNS, \
-  KC_LCTRL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                       XXXXXXX,   KC_P4,    KC_P5,     KC_P6,    KC_PCMM,  XXXXXXX, \
-  KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, _______,     _______, XXXXXXX,   KC_P1,    KC_P2,     KC_P3,    KC_EQL,  KC_TRNS, \
+  KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                       XXXXXXX,   KC_P4,    KC_P5,     KC_P6,    KC_PCMM,  XXXXXXX, \
+  KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, _______,     _______, XXXXXXX,   KC_P1,    KC_P2,     KC_P3,    KC_EQL,  KC_RSFT, \
         _______, _______, _______, _______, KC_SPC,                 KC_ESC, KC_P0, KC_PDOT, TO(_QWERTY), KC_PENT\
 ),
 /* SYMB
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYMB] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                         KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                       KC_R,  KC_E,  KC_PSCR, KC_SLCK, KC_PAUS, KC_F12, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,                       KC_R,  KC_E,  KC_PSCR, KC_SCRL, KC_PAUS, KC_F12, \
   _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, XXXXXXX,                       KC_I,  KC_S,  KC_INS,  KC_HOME, KC_PGUP, KC_BSPC, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  _______,    _______, KC_U,  KC_B,  KC_DEL,  KC_END,  KC_PGDN, _______, \
         _______, TO(_QWERTY), _______, _______, KC_ENT,                      KC_SPC, _______, _______, _______, _______ \
@@ -133,7 +133,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 #ifdef ENCODER_ENABLE
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
             tap_code(KC_VOLU);
@@ -142,11 +142,12 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDOWN);
+            tap_code(KC_PGDN);
         } else {
             tap_code(KC_PGUP);
         }
     }
+    return false;
 }
 
 #endif
